f52f130744faf9e66a0dcce722b7e5f81e5ad46e
haw230 <hancheng.wu22@gmail.com>
2017-07-26 15:30:12 -0600

    Update bubble_gif.py

--- src/bubble_gif.py
+++ src/bubble_gif.py
@@ -1,23 +1,23 @@
 import pylab as plt
 from random import shuffle
 
-def bubblesort_anim(a):
- x = range(len(a))
- imgidx = 0
- # bubble sort algorithm
- swapped = True
- while swapped: # until there's no swapping
-  swapped = False
-  for i in range(len(a)-1):
-   if a[i] > a[i+1]:
-    a[i+1], a[i] = a[i], a[i+1] # swap
+def bubblesort_anim(ls):
+    x = range(len(ls))
+    imgidx = 0
     swapped = True
-  plt.plot(x,a,'ro',markersize=6)
-  plt.savefig("/cshome/ha10/Pictures/" + '%04d' % imgidx + ".png")
-  plt.clf() # figure clear
-  imgidx += 1
+    while swapped:
+        swapped = False
+        for i in range(len(ls) - 1):
+            if ls[i] > ls[i + 1]:
+                (ls[i + 1], ls[i]) = (a[i], ls[i + 1])
+                swapped = True
+        plt.plot(x, ls, 'ro', markersize=6)
+        plt.savefig('/cshome/ha10/Pictures/' + '%04d' % imgidx + '.png')
+        plt.clf()
+        imgidx += 1
 
-# running the algorithm
 a = list(range(300))
-shuffle(a)
-bubblesort_anim(a)
+shuffle(ls)
+bubblesort_anim(ls)
+
+			
