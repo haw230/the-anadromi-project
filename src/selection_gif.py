13a412b33be2aac3068804035439ca4484c22921
haw230 <hancheng.wu22@gmail.com>
2017-07-26 15:28:43 -0600

    Update selection_gif.py

--- src/selection_gif.py
+++ src/selection_gif.py
@@ -1,21 +1,19 @@
 import pylab as plt
 from random import shuffle
 
-def selectionsort_anim(a):
- x = range(len(a)) 
- for j in range(len(a)-1):
-  iMin = j
-  for i in range(j+1,len(a)):
-   if a[i] < a[iMin]: # find the smallest value
-    iMin = i
-  if iMin != j: # place the value into its proper location
-   a[iMin], a[j] = a[j], a[iMin]
-  # plotting
-  plt.scatter(x,a,color='g',marker='.')
-  plt.savefig("/cshome/ha10/Pictures/" + '%04d' % j + ".png")
-  plt.clf() # figure clear
-
-# running the algorithm
-a = list(range(300)) # initialization of the array
-shuffle(a)     # shuffle!
-selectionsort_anim(a) # sorting
+def selectionsort_anim(ls):
+    x = range(len(ls))
+    for i in range(len(ls) - 1):
+        lowest = i
+        for j in range(i + 1, len(ls)):
+            if ls[j] < ls[lowest]:
+                lowest = j
+        ls.insert(i, ls[lowest])
+        del a[lowest + 1]
+        plt.scatter(x, a, color='g', marker='.')
+        plt.savefig('/cshome/ha10/Pictures/' + '%04d' % i + '.png')
+        plt.clf()
+        
+a = list(range(300))
+shuffle(ls)
+selectionsort_anim(ls)
